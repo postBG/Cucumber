b040c2ef3cda101aea23e0f1d2a7ca70ac9d9c44
jaehoon <ojh420@gmail.com>
2015-11-16 16:07:34 +0900

    Cucumber Test Runner 에서 feature 파일 경로 지정

--- integration/com/inchon/parking/ShortTermParkingLotTest.java
+++ integration/com/inchon/parking/ShortTermParkingLotTest.java
@@ -2,9 +2,11 @@ package com.inchon.parking;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(features={"integration/com/inchon/parking/ShortTermParkingLot.feature"})
 public class ShortTermParkingLotTest {
 
 }

b5c976a076bcf324737f17aaf6714ae7512c7fc9
jaehoon <ojh420@gmail.com>
2015-11-17 13:59:16 +0900

    [Green] 평일 1분, 1200원

--- src/com/inchon/parking/ParkingCalculator.java
+++ src/com/inchon/parking/ParkingCalculator.java
@@ -3,6 +3,8 @@ package com.inchon.parking;
 public class ParkingCalculator {
 
 	public Integer calculate(Integer durationInMinutes) {
+		if ( durationInMinutes == 1 )
+			return 1200;
 		return 0;
 	}
 

f9d787f3a46f2a76e4a116b71d8810d370c8182d
jaehoon <ojh420@gmail.com>
2015-11-17 14:41:20 +0900

    [Acceptance Test]

--- integration/com/inchon/parking/ShortTermParkingLotStepDefinitions.java
+++ integration/com/inchon/parking/ShortTermParkingLotStepDefinitions.java
@@ -15,19 +15,34 @@ public class ShortTermParkingLotStepDefinitions {
 	private Map<String,TimeInterval> durationMap = new HashMap<String,TimeInterval>() {
 		{
 			put("1분",new TimeInterval("2015-11-16 00:00:00","2015-11-16 00:01:00"));
+			put("30분",new TimeInterval("2015-11-16 00:00:00","2015-11-16 00:30:00"));
+			put("31분",new TimeInterval("2015-11-16 00:00:00","2015-11-16 00:31:00"));
+			put("45분",new TimeInterval("2015-11-16 00:00:00","2015-11-16 00:45:00"));
+			put("1시간",new TimeInterval("2015-11-16 00:00:00","2015-11-16 01:00:00"));
+			put("5시간",new TimeInterval("2015-11-16 00:00:00","2015-11-16 05:00:00"));
+			put("5시간 1분",new TimeInterval("2015-11-16 00:00:00","2015-11-16 05:00:00"));
+			put("1일",new TimeInterval("2015-11-16 00:00:00","2015-11-17 00:00:00"));
+			put("1일 1분",new TimeInterval("2015-11-16 00:00:00","2015-11-17 00:01:00"));
+			put("1일 31분",new TimeInterval("2015-11-16 00:00:00","2015-11-17 00:31:00"));
+			put("2일 1분",new TimeInterval("2015-11-16 00:00:00","2015-11-18 00:1:00"));
+			put("3일 1분",new TimeInterval("2015-11-16 00:00:00","2015-11-19 00:1:00"));
 		}
 	};
 	private Integer durationInMinutes;
+	private String durationType;
 
 	@When("^나는 평일에 단기 주차장에 (.*)동안 주차한다\\.$")
 	public void 나는_평일에_단기_주차장에_동안_주차한다(String duration) throws Throwable {
 		TimeInterval timeInterval = durationMap.get(duration);
 		durationInMinutes = timeInterval.durationInMinutes();
+		durationType = "평일";
 	}
 	
 	@When("^나는 주말에 단기 주차장에 (.*)동안 주차한다\\.$")
 	public void 나는_주말에_단기_주차장에_동안_주차한다(String duration) throws Throwable {
-		throw new PendingException();
+		TimeInterval timeInterval = durationMap.get(duration);
+		durationInMinutes = timeInterval.durationInMinutes();
+		durationType = "주말";
 	}	
 
 	@When("^나는 주말과 평일 혼합하여 단기 주차장에 (.*)동안 주차한다\\.$")
@@ -37,7 +52,13 @@ public class ShortTermParkingLotStepDefinitions {
 	
 	@Then("^나는 주차요금으로 (\\d+)원을 지불해야 한다\\.$")
 	public void 나는_주차요금으로_원을_지불해야_한다(int expectedPrice) throws Throwable {
-		ParkingCalculator parkingCalculator = new ParkingCalculator();
-		assertThat(parkingCalculator.calculate(durationInMinutes),is(expectedPrice));
+		if ( "평일".equals(durationType) ) {
+			ParkingCalculator parkingCalculator = new ParkingCalculator();
+			assertThat(parkingCalculator.calculate(durationInMinutes),is(expectedPrice));
+		}
+		else {
+			ParkingCalculator parkingCalculator = ParkingCalculator.createWeekendParkingCalculator();
+			assertThat(parkingCalculator.calculate(durationInMinutes),is(expectedPrice));			
+		}
 	}
 }
--- src/com/inchon/parking/ParkingCalculator.java
+++ src/com/inchon/parking/ParkingCalculator.java
@@ -1,26 +1,39 @@
 package com.inchon.parking;
 
 public class ParkingCalculator {
+	private static final int DAY_IN_MINUTES = 24*60;
+	
+	private int 	dailyCostLimit = 12000;
+	private int additionalCost = 600;
+	private int baseCost = 1200;
+	private int dailyCostLimitDuration = 5*60;
+	private int baseCostDuration = 30;
+	private int additionalCostTimeUnit = 15;
 
 	public Integer calculate(Integer durationInMinutes) {
 		int cost = 0;
 		
-		int days = durationInMinutes/(24*60);
+		int days = durationInMinutes/DAY_IN_MINUTES;
 		if ( days >= 1 ) {
-			cost += days*12000;
-			durationInMinutes -= days*24*60;
+			cost += days*dailyCostLimit;
+			durationInMinutes -= days*DAY_IN_MINUTES;
 		}
 		
-		if ( durationInMinutes >= 5*60 ) {
-			cost += 12000;
+		if ( durationInMinutes >= dailyCostLimitDuration ) {
+			cost += dailyCostLimit;
 			durationInMinutes = 0;
 		}
-		if ( durationInMinutes >= 31 ) {
-			cost += ((durationInMinutes-30-1)/15+1)*600;
+		if ( durationInMinutes > baseCostDuration ) {
+			cost += ((durationInMinutes-baseCostDuration-1)/additionalCostTimeUnit+1)*additionalCost;
 		}
-		if ( durationInMinutes >= 1 )
-			cost += 1200;
+		if ( durationInMinutes >= 1 ) {
+			cost += baseCost;
+		}
 		return cost;
+	}
+	
+	public static ParkingCalculator createWeekendParkingCalculator() {
+		return new ParkingCalculator();
 	}
 
 }

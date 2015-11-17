d559a5a6f7f2d244a7931ae1da3877709bbf78a9
jaehoon <ojh420@gmail.com>
2015-11-17 14:06:31 +0900

    [Refactor] 비즈니스 로직 도출

--- src/com/inchon/parking/ParkingCalculator.java
+++ src/com/inchon/parking/ParkingCalculator.java
@@ -3,11 +3,14 @@ package com.inchon.parking;
 public class ParkingCalculator {
 
 	public Integer calculate(Integer durationInMinutes) {
-		if ( durationInMinutes >= 31 )
-			return 1200 + 600;
+		int cost = 0;
+		
+		if ( durationInMinutes >= 31 ) {
+			cost += 600;
+		}
 		if ( durationInMinutes >= 1 )
-			return 1200;
-		return 0;
+			cost += 1200;
+		return cost;
 	}
 
 }

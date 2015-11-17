3931fa7521e60ef249d91808e3a48d27c5b05fc9
jaehoon <ojh420@gmail.com>
2015-11-17 18:37:00 +0900

    [Refactoring] durationList 테스트 케이스를 ParkingDurationGeneratorTest 옮기기

--- test/com/inchon/parking/TimeIntervalTest.java
+++ test/com/inchon/parking/TimeIntervalTest.java
@@ -10,25 +10,6 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class TimeIntervalTest {
-	private TimeService timeService = new TimeService() {
-		List<DateTime> weekends = new ArrayList<DateTime>() {
-			{
-				add(new DateTime(2015,11,17,0,0));
-				add(new DateTime(2015,11,19,0,0));
-			}
-		};
-		
-		public boolean isWeekend(DateTime date) {
-			DateTime queryDate = date.withTime(0, 0, 0, 0);
-			for ( DateTime weekend : weekends ) {
-				if ( weekend.equals(queryDate) ) {
-					return true;
-				}
-			}
-			return false;
-		};
-	};
-
 	@Test
 	public void durationLessThanHour() {
 		TimeInterval timeInterval = new TimeInterval("2015-11-16 00:00:00", "2015-11-16 00:01:00");
@@ -40,66 +21,4 @@ public class TimeIntervalTest {
 		TimeInterval timeInterval = new TimeInterval("2015-11-16 00:00:00", "2015-11-16 01:01:00");
 		assertThat(timeInterval.durationInMinutes(),is(61));
 	}
-	
-	@Test
-	public void durationListForWeekdayOnly() {
-		TimeInterval timeInterval = new TimeInterval("2015-11-16 00:00:00", "2015-11-16 01:01:00");
-		List<ParkingDuration> durations = timeInterval.durationList(timeService, new ParkingDurationGenerator());
-		assertThat(durations.size(),is(1));
-		assertThat(durations.get(0).getDurationInMinutes(),is(61));
-		assertThat(durations.get(0).isWeekend(),is(false));
-	}	
-	
-	@Test
-	public void durationListForTwoWeekdays() {
-		TimeInterval timeInterval = new TimeInterval("2015-11-11 00:00:00", "2015-11-13 01:01:00");
-		List<ParkingDuration> durations = timeInterval.durationList(timeService, new ParkingDurationGenerator());
-		assertThat(durations.size(),is(1));
-		assertThat(durations.get(0).getDurationInMinutes(),is(2*24*60+61));
-		assertThat(durations.get(0).isWeekend(),is(false));
-	}
-	
-	@Test
-	public void durationListForWeekEndOnly() {
-		TimeInterval timeInterval = new TimeInterval("2015-11-17 00:00:00", "2015-11-17 01:01:00");
-		List<ParkingDuration> durations = timeInterval.durationList(timeService, new ParkingDurationGenerator());
-		assertThat(durations.size(),is(1));
-		assertThat(durations.get(0).getDurationInMinutes(),is(61));
-		assertThat(durations.get(0).isWeekend(),is(true));
-	}	
-
-	@Test
-	public void durationForMixedDays() {
-		TimeInterval timeInterval = new TimeInterval("2015-11-16 23:59:00", "2015-11-17 00:01:00");
-		List<ParkingDuration> durations = timeInterval.durationList(timeService, new ParkingDurationGenerator());
-		assertThat(durations.size(),is(2));
-		assertThat(durations.get(0).getDurationInMinutes(),is(1));
-		assertThat(durations.get(0).isWeekend(),is(false));
-		assertThat(durations.get(1).getDurationInMinutes(),is(1));
-		assertThat(durations.get(1).isWeekend(),is(true));
-	}
-	
-	@Test
-	public void durationForTwoMixedDays() {
-		TimeInterval timeInterval = new TimeInterval("2015-11-15 23:59:00", "2015-11-17 00:01:00");
-		List<ParkingDuration> durations = timeInterval.durationList(timeService, new ParkingDurationGenerator());
-		assertThat(durations.size(),is(2));
-		assertThat(durations.get(0).getDurationInMinutes(),is(24*60+1));
-		assertThat(durations.get(0).isWeekend(),is(false));
-		assertThat(durations.get(1).getDurationInMinutes(),is(1));
-		assertThat(durations.get(1).isWeekend(),is(true));
-	}		
-	
-	@Test
-	public void durationForTwoAnyMixedDays() {
-		TimeInterval timeInterval = new TimeInterval("2015-11-16 23:59:00", "2015-11-18 00:01:00");
-		List<ParkingDuration> durations = timeInterval.durationList(timeService, new ParkingDurationGenerator());
-		assertThat(durations.size(),is(3));
-		assertThat(durations.get(0).getDurationInMinutes(),is(1));
-		assertThat(durations.get(0).isWeekend(),is(false));
-		assertThat(durations.get(1).getDurationInMinutes(),is(24*60));
-		assertThat(durations.get(1).isWeekend(),is(true));
-		assertThat(durations.get(2).getDurationInMinutes(),is(1));
-		assertThat(durations.get(2).isWeekend(),is(false));
-	}		
 }
